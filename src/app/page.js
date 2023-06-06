595e44ee87b625d4bbb40edd08fc2eedd9606f17
Daniel <daniel.nastase@gmail.com>
2023-06-06 14:39:20 +0300

    observer ready

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { useInfiniteQuery, QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useState, useRef, useEffect } from "react"
 
 const fetchData = async (page) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=27&_page=' + page)
@@ -11,6 +12,18 @@ const fetchData = async (page) => {
 const queryClient = new QueryClient();
 
 const MyComponent = ()=> {
+  const myRef = useRef(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(()=> {
+    const observer = new IntersectionObserver( (entries) => {
+        entries.forEach((entry)=> {
+            console.log("is on screen = " + entry.isIntersecting)
+        });
+    });
+    if (myRef.current) observer.observe(myRef.current)
+  }, [myRef])
+
   const {data, fetchNextPage, isFetchingNextPage} = useInfiniteQuery(
     ['query'], 
     async ({pageParam = 1}) => {
@@ -45,6 +58,7 @@ const MyComponent = ()=> {
       ))}
     </ol>
     <button 
+      ref={myRef}
       onClick={() => fetchNextPage()}
       disabled={isFetchingNextPage}>
         {loadMoreBtnText()}
